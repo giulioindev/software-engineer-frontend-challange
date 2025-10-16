6f29eb83206bda7d2857fe64fff3ed9e8d7c0c6e
Giulio Indennitate <giulio.indennitate@gmail.com>
2025-10-16 16:20:13 +0200

    feat: add boxShadow for card and table

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,18 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-  colorSchemes: { light: true, dark: true },
+  colorSchemes: {
+    light: {
+      palette: {
+        mode: "light",
+      },
+    },
+    dark: {
+      palette: {
+        mode: "dark",
+      },
+    },
+  },
   cssVariables: {
     colorSchemeSelector: "class",
   },
@@ -22,6 +33,22 @@ const theme = createTheme({
               },
             },
           ],
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: "10px",
+          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+        },
+      },
+    },
+    MuiTable: {
+      styleOverrides: {
+        root: {
+          borderRadius: "10px",
+          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
         },
       },
     },
